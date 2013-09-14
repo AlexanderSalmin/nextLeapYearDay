16bfd02fb3a7acd7d9710a958057ca741eb66e94
Alexander Salmin <alexander@salmin.biz>
2013-09-14 16:56:23 +0200

    added basic functions

--- nextLeapYearDay.rb
+++ nextLeapYearDay.rb
@@ -3,3 +3,16 @@
 year = Time.now().year
 maxyear = year + 50
 
+def wday(day)
+ days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+ return days[day]
+end
+		
+
+while year < maxyear
+	curdate = Time.new(year,2,29)
+	if curdate.day == 29
+		puts year.to_s + ": " + wday(curdate.wday)
+	end
+	year = year + 1
+end
