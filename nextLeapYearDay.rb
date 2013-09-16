f50494136ebee0b4aa7dce427a0baf6dc78acd7e
Alexander Salmin <alexander.salmin@outlook.com>
2013-09-16 17:24:31 +0200

    Removed ugly function

--- nextLeapYearDay.rb
+++ nextLeapYearDay.rb
@@ -2,16 +2,12 @@
 
 year = Time.now().year
 maxyear = year + 50
-
-def wday(day)
- days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
- return days[day]
-end
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 		
 while year < maxyear
 	curdate = Time.new(year,2,29)
 	if curdate.day == 29
-		puts year.to_s + ": " + wday(curdate.wday)
+		puts year.to_s + ": " + days[curdate.wday]
 	end
 	year = year + 1
 end
